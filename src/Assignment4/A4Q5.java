70da3d5118c75c9c947c6838016e578d18f3d681
droel8940 <droel8940@HRH-IC0027336.wrdsb.ca>
2017-10-13 14:16:30 -0400

    October 13, 2017

--- src/Assignment4/A4Q5.java
+++ src/Assignment4/A4Q5.java
@@ -18,11 +18,41 @@ public class A4Q5 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //Ask the user to type their name
-        System.out.println("Please enter your name: ");
-        
         //create scanner for input
         Scanner input = new Scanner(System.in);
+        
+          //Ask the user how much the food cost
+        System.out.println("Please enter your name: ");
+        
+        //create a string for the first variable 
+        int userName = input.nextInt();
+        
+        //Ask the user how much the DJ cost
+        System.out.println("What was the first test out of?: ");
+        
+        //create a string for the second variable 
+        int testOne = input.nextInt();
+        
+        //Ask the user how much the Hall cost
+        System.out.println("What mark did you get?: ");
+        
+        //create a string for the third variable 
+        int thirdCost = input.nextInt();
+        
+        //Ask the user how much the Decorations cost
+        System.out.println("How much does decorations cost?: ");
+        
+        //create a string for the fourth variable 
+        int fourthCost = input.nextInt();
+        
+        //Ask the user how much the staff cost
+        System.out.println("How much does it cost for staff?: ");
+        
+        //create a string for the fifth variable 
+        int fifthCost = input.nextInt();
+        
+        
+        
         
         
         
--- src/Quiz1/Quiz1.java
+++ src/Quiz1/Quiz1.java
@@ -21,65 +21,81 @@ public class Quiz1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //create City
         City kw = new City();
-        
+
         //create walls of mine
-       new Wall(kw, 1, 1, Direction.WEST);
-       new Wall(kw, 1, 4, Direction.NORTH);
-       new Wall(kw, 1, 3, Direction.NORTH);
-       new Wall(kw, 1, 5, Direction.NORTH);
-       new Wall(kw, 1, 6, Direction.NORTH);
-       new Wall(kw, 1, 7, Direction.NORTH);
-       new Wall(kw, 1, 7, Direction.EAST);
-       new Wall(kw, 1, 7, Direction.SOUTH);
-       new Wall(kw, 1, 6, Direction.SOUTH);
-       new Wall(kw, 1, 5, Direction.SOUTH);
-       new Wall(kw, 1, 4, Direction.SOUTH);
-       new Wall(kw, 1, 3, Direction.SOUTH);
-       
-       //create Things
-       new Thing(kw, 1, 3, Direction.SOUTH);
-       new Thing(kw, 1, 4, Direction.SOUTH);
-       new Thing(kw, 1, 5, Direction.SOUTH);
-       new Thing(kw, 1, 6, Direction.SOUTH);
-       new Thing(kw, 1, 7, Direction.SOUTH);
-       
-       //create a miner(robot)
-       Robot bob = new Robot(kw, 1, 1, Direction.EAST);
-       
-       kw.showThingCounts(true);
-       
-       while(!(bob.canPickThing()) && bob.frontIsClear()){
-           bob.move();
-           
-           while(bob.canPickThing()){
-           bob.pickThing();
-           bob.turnLeft();
-           bob.turnLeft();
-           
-           while(bob.getAvenue() != 1 && bob.getDirection() == Direction.WEST){
-           bob.move();
-           
-           }
-           while(!(bob.frontIsClear())){
-               bob.turnLeft();
-               bob.turnLeft();
-           }
-               
-           
-       }
-       
-       
-             
-       }
-       
-       }
-           
-    
-        }   
-    
-    
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        new Wall(kw, 1, 6, Direction.NORTH);
+        new Wall(kw, 1, 7, Direction.NORTH);
+        new Wall(kw, 1, 7, Direction.EAST);
+        new Wall(kw, 1, 7, Direction.SOUTH);
+        new Wall(kw, 1, 6, Direction.SOUTH);
+        new Wall(kw, 1, 5, Direction.SOUTH);
+        new Wall(kw, 1, 4, Direction.SOUTH);
+        new Wall(kw, 1, 3, Direction.SOUTH);
+
+        //create Things
+        new Thing(kw, 1, 3, Direction.SOUTH);
+        new Thing(kw, 1, 4, Direction.SOUTH);
+        new Thing(kw, 1, 5, Direction.SOUTH);
+        new Thing(kw, 1, 6, Direction.SOUTH);
+        new Thing(kw, 1, 7, Direction.SOUTH);
+
+        //create a miner(robot)
+        Robot bob = new Robot(kw, 1, 1, Direction.EAST);
+
+        //turn on thing counter
+        kw.showThingCounts(true);
+
+        //make bob walk to thing
+        while (!(bob.canPickThing()) && bob.frontIsClear()) {
+            bob.move();
+
+            //make bob pick thing and turn around
+            while (bob.canPickThing()) {
+                bob.pickThing();
+                bob.turnLeft();
+                bob.turnLeft();
+
+                //make bob go back to his starting place
+                while (bob.getAvenue() != 1 && bob.getDirection() == Direction.WEST) {
+                    bob.move();
+
+                    if (!(bob.frontIsClear())) {
+                        bob.putThing();
+                    }
+
+                    while (!(bob.frontIsClear())) {
+                        bob.turnLeft();
+                        bob.turnLeft();
+                        bob.move();
+                    }
+
+                }
+
+            }
 
 
+
+        }
+        //make bob face WEST
+        while (bob.getDirection() != Direction.WEST) {
+            bob.turnLeft();
+        }
+
+        //make bob go back to his starting place
+        while (bob.getAvenue() != 1 && bob.getDirection() == Direction.WEST) {
+            bob.move();
+        }
+
+        //make bob face EAST
+        while (bob.getDirection() != Direction.EAST) {
+            bob.turnLeft();
+        }
+    }
+}           //The End
