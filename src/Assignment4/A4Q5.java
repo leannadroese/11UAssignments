a7668df15d3715897f7db1c9028ac35bd81cce6c
droel8940 <droel8940@HRH-IC0027336.wrdsb.ca>
2017-10-16 13:19:07 -0400

    October 16, 2017

--- src/Assignment4/A4Q5.java
+++ src/Assignment4/A4Q5.java
@@ -23,38 +23,116 @@ public class A4Q5 {
         
           //Ask the user how much the food cost
         System.out.println("Please enter your name: ");
-        
+
         //create a string for the first variable 
-        int userName = input.nextInt();
+        String userName = input.nextLine();
         
-        //Ask the user how much the DJ cost
+        //Ask the user what the first test was out of
         System.out.println("What was the first test out of?: ");
         
-        //create a string for the second variable 
-        int testOne = input.nextInt();
+        //
+        double testOne = input.nextDouble();
         
-        //Ask the user how much the Hall cost
+        //Ask the user what his test mark was
         System.out.println("What mark did you get?: ");
         
-        //create a string for the third variable 
-        int thirdCost = input.nextInt();
+        // 
+        double markOne = input.nextDouble();
         
-        //Ask the user how much the Decorations cost
-        System.out.println("How much does decorations cost?: ");
+        //Ask the user what the second test was out of
+        System.out.println("What was the second test out of?: ");
         
-        //create a string for the fourth variable 
-        int fourthCost = input.nextInt();
+        //
+        double testTwo = input.nextDouble();
         
-        //Ask the user how much the staff cost
-        System.out.println("How much does it cost for staff?: ");
+        //Ask the user what their mark on the second test was
+        System.out.println("What mark did you get?: ");
         
-        //create a string for the fifth variable 
-        int fifthCost = input.nextInt();
+        //
+        double markTwo = input.nextDouble();
         
+        //Ask the user what the first test was out of
+        System.out.println("What was the third test out of?: ");
         
+        //
+        double testThree = input.nextDouble();
         
+        //Ask the user what his test mark was
+        System.out.println("What mark did you get?: ");
         
+        // 
+        double markThree = input.nextDouble();
         
+        //Ask the user what the first test was out of
+        System.out.println("What was the fourth test out of?: ");
+        
+        //
+        double testFour = input.nextDouble();
+        
+        //Ask the user what his test mark was
+        System.out.println("What mark did you get?: ");
+        
+        // 
+        double markFour = input.nextDouble();
+        
+        //Ask the user what the first test was out of
+        System.out.println("What was the fifth test out of?: ");
+        
+        //
+        double testFive = input.nextDouble();
+        
+        //Ask the user what his test mark was
+        System.out.println("What mark did you get?: ");
+        
+        // 
+        int markFive = input.nextInt();
+        
+        //make a space between lines
+        System.out.println(      );
+        
+        //Tell the user their test scores
+        System.out.println("Test Scores for " + userName + ":");
+        
+        //find the users percentage for the first test
+        double firstTest = markOne / testOne;
+        double firstFinal = firstTest * 100;
+        //output the information
+        System.out.println("Test 1: " + firstFinal + "%");
+        
+        //find the users percentage for the second test
+        double secondTest = markTwo / testTwo;
+        double secondFinal = secondTest * 100;
+        //output the information
+        System.out.println("Test 2: " + secondFinal + "%");
+        
+        //find the users percentage for the third test
+        double thirdTest = markThree / testThree;
+        double thirdFinal = thirdTest * 100;
+        //output the information
+        System.out.println("Test 3: " + thirdFinal + "%");
+        
+        //find the users percentage for the fourth test
+        double fourthTest = markFour / testFour;
+        double fourthFinal = fourthTest * 100;
+        //output the information
+        System.out.println("Test 4: " + fourthFinal + "%");
+        
+        //find the users percentage for the fifth test
+        double fifthTest = markFive / testFive;
+        double fifthFinal = fifthTest * 100;
+        //output the information
+        System.out.println("Test 5: " + secondFinal + "%");
+           
+        //make a space between lines
+        System.out.println(      );
+        
+        //find the users overall average
+        double totalAdding = firstFinal + secondFinal + thirdFinal + fourthFinal + fifthFinal;
+        double totalAverage = totalAdding / 5;
+        //output the information
+        System.out.println("Average: " + totalAverage + "%");
+        
+        //The End
         
     }
 }
---
+++ src/Assignment4/A4Q6.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author droel8940
+ */
+public class A4Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //create scanner for input
+        Scanner input = new Scanner(System.in);
+        
+        //Ask the user what the first test was out of
+        System.out.println("What is the speed limit?: ");
+        
+        // Create int for speed limit
+        int speedLimit = input.nextInt();
+        
+        //Ask the user what his test mark was
+        System.out.println("Enter the recorded speed of the car?: ");
+        
+        // Create int for drivers speed
+        int driverSpeed = input.nextInt();
+        
+        if(speedLimit > driverSpeed){
+            System.out.println("Congrats! You are within the speed limit!");
+            
+            if the 
+            
+        }
+    }
+}
