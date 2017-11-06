7cc06b3503956744e560bcf2faba75fd1020290b
droel8940 <droel8940@HRH-IC0027336.wrdsb.ca>
2017-11-06 13:09:08 -0500

    November 6, 2017

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -16,51 +16,79 @@ public class A5Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         //create a scanner
         Scanner input = new Scanner(System.in);
-        
+
         //ask the user to input a word
         System.out.println("Enter a word: ");
         String word = input.nextLine();
-        
+
         //convert word to lower case
         word = word.toLowerCase();
-        
-        //put ub in infront of the word if it starts with a vowel
-       if(word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u")){
-           //create a string
-           String translated = "ub" + word;
-           //output to user
-           System.out.println(translated);
-       }
-            //not a vowel, special rule time
-            //find the first vowel by going through each letter
-            for (int i = 0; i < word.length(); i++) {
-                //look for vowel at spot i
-                if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
-                    
-                    String front = word.substring(0, i);
-                    String ending = word.substring(i);
-                   
-                    String translatedd = front + "ub" + ending;
-                    
-                    System.out.println(translatedd);
-                   
-                    break;
-     
-      }
-       
+
+        //create String for translated word
+        String translate = new String();
+        //create String for letter a
+        String a = new String("a");
+        //create String for letter e
+        String e = new String("e");
+        //create String for letter i
+        String i = new String("i");
+        //create String for letter o
+        String o = new String("o");
+        //create String for letter u
+        String u = new String("u");
+        //create String for letters oo
+        String oo = new String("oo");
+        //create String for letters ou
+        String ou = new String("ou");
+        //create String for letters au
+        String au = new String("au");
+
+        //if the word does not have oo or ou, translate it with this 
+        if (!(word.contains(oo)) || (!(word.contains(ou)))) {
+
+            //replace the letter u with ubu
+            if (word.contains(a) || word.contains(e) || word.contains(i) || word.contains(o) || word.contains(u)) {
+                String transU = word.replaceAll(u, "ubu");
+                String translated = new String(transU);
+
+                //replace the letter a with uba
+                if (translated.contains(a) || translated.contains(e) || translated.contains(i) || translated.contains(i) || translated.contains(o) || translated.contains(u)) {
+                    String transA = transU.replaceAll(a, "uba");
+                    String translatedA = new String(transA);
+
+                    //replace the letter e with ube
+                    if (translatedA.contains(a) || translatedA.contains(e) || translatedA.contains(i) || translatedA.contains(o) || translatedA.contains(u)) {
+                        String transE = translatedA.replaceAll(e, "ube");
+                        String translatedE = new String(transE);
+
+                        //replace the letter i with ubi
+                        if (translatedE.contains(a) || translatedE.contains(e) || translatedE.contains(i) || translatedE.contains(o) || translatedE.contains(u)) {
+                            String transI = translatedE.replaceAll(i, "ubi");
+                            String translatedI = new String(transI);
+
+                            //replace the letter o with ubo
+                            if (translatedI.contains(a) || translatedI.contains(e) || translatedI.contains(i) || translatedI.contains(o) || translatedI.contains(u)) {
+                                String transO = translatedI.replaceAll(o, "ubo");
+                                String translatedO = new String(transO);
+
+                                System.out.println(translatedO);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (word.contains(oo)) {
+            String doubleO = word.replaceAll(oo, "uboo");
+            System.out.println(doubleO);
+
+        }
+        if (word.contains(ou)) {
+            String doubleOU = word.replaceAll(ou, "ubou");
+            System.out.println(doubleOU);
+        }
     }
-       }
 }
-    
-
-        
-    
-           
-       
-       
-        
-    
-
