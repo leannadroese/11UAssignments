6ff5e332905f0b42050f0774db5585d6535f2caf
droel8940 <droel8940@HRH-IC0027336.wrdsb.ca>
2017-11-30 13:01:34 -0500

    November 30, 2017

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -4,6 +4,8 @@
  */
 package Assignment6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author droel8940
@@ -14,6 +16,25 @@ public class A6Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Create a scanner
+        Scanner input = new Scanner(System.in);
+        
+        //Create an array
+        int[] array = new int[1000];
+        
+        //Make the first space in the array = 1
+        array[0] = 1;
+        
+        //Put numbers into array
+        for (int i = 0; i < array.length; i++){
+            //make another int that will do the adding with the i
+            for (int j =1; j < array.length - 1; j++){
+             array[j]= array[i] + 1;   
+            }
+            
+            
+        }
+        System.out.println(array[22]);
     }
+    
 }
