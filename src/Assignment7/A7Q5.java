e8a0961d8feb001169ecf097ce94b91089658b6a
droel8940 <droel8940@HRH-IC0027336.wrdsb.ca>
2017-12-15 13:32:37 -0500

    December 15, 2017

--- src/Assignment7/A7Q5.java
+++ src/Assignment7/A7Q5.java
@@ -13,27 +13,32 @@ import java.util.Scanner;
 public class A7Q5 {
 
     public void chaotic(int stars) {
-
+        //If stars is not equal to 5.....which it never will be....follow this loop
         while (stars == 5) {
 
             int randomNum = (int) (Math.random() * (5 - 1 + 1)) + 1;
 
+            //If the random number is 1 output one star
             if (randomNum == 1) {
                 System.out.println("*");
             }
 
+            //If the random number is 2 output two star
             if (randomNum == 2) {
                 System.out.println("**");
             }
 
+            //If the random number is 3 output three star
             if (randomNum == 3) {
                 System.out.println("***");
             }
 
+            //If the random number is 4 output four star
             if (randomNum == 4) {
                 System.out.println("****");
             }
 
+            //If the random number is 5 output five star
             if (randomNum == 5) {
                 System.out.println("*****");
             }
